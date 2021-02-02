bd61e4884dc924a859a6eedbe9aefe7481c222a8
shihabmilky1 <shihabmilky1@gmail.com>
2021-02-02 22:13:02 +0600

    truth vs false

---
+++ true-falsy.js
@@ -0,0 +1,17 @@
+const name = '';
+if (name) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+//Falsy
+//0
+//undefined
+//null
+//""
+//false
+//NaN
+// ============================
+//Truthy
+//1
+//" "
