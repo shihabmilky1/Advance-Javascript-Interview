5213c480d0b20fa0d1b2198d57a010ef9b9cec81
shihabmilky1 <shihabmilky1@gmail.com>
2021-02-02 22:37:48 +0600

    null vs undefined interview question

---
+++ null-vs-undefiend.js
@@ -0,0 +1,21 @@
+undefined
+let paki;
+console.log(paki);
+
+function add(num1,num2){
+    console.log(num1 , num2);
+}
+const result = add (10,20);
+console.log(result);
+
+function add(num1,num2){
+    console.log(num1 , num2);
+}
+const result = add (20);
+console.log(result);
+
+const object = {name:"shihab" , number:1235656};
+console.log(object.address);
+
+let access = undefined;
+console.log(access);
